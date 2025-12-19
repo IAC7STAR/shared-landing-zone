39391a27b1ea3505ac224463fdd444ecb96c16aa
IAC7STAR <iacsevenstar@gmail.com>
2025-12-19 17:26:52 +0530

    Add missing .tf files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.0"  # Good choice; this is compatible with most recent features
     }
   }
 }
 
-provider "azurerm {
-  features {}
+provider "azurerm" {  # ← Add the closing " after azurerm
+  features {}         # Required for AzureRM provider (can be empty or with options)
 
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
